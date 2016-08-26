4fdf8aeb6501c1ff15b0a8599f4abe3c1aa35336
dddunker <mb@audienceproject.com>
2016-08-26 17:39:48 +0300

    refactoring

--- src/main/java/com/mbi/AssertionDirector.java
+++ src/main/java/com/mbi/AssertionDirector.java
@@ -7,6 +7,9 @@ import org.json.JSONObject;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 /**
  * Created by mbi on 8/19/16.
  */
@@ -28,39 +31,29 @@ class AssertionDirector {
         // JSONArray - JSONArray
         if (actual instanceof JSONArray && expected instanceof JSONArray) {
             assertEquals((JSONArray) expected, (JSONArray) actual, mode, ignore);
-
-            // JSONObject - JSONObject
-        } else if (actual instanceof JSONObject && expected instanceof JSONObject) {
+        }
+        // JSONObject - JSONObject
+        else if (actual instanceof JSONObject && expected instanceof JSONObject) {
             assertEquals((JSONObject) expected, (JSONObject) actual, mode, ignore);
-
-            // JSONArray - JSONObject[]
-        } else if (actual instanceof JSONArray && expected instanceof JSONObject[]) {
+        }
+        // JSONArray - JSONObject[]
+        else if (actual instanceof JSONArray && expected instanceof JSONObject[]) {
             assertEquals(objectsToArray((JSONObject[]) expected), (JSONArray) actual, mode, ignore);
-
-            // Response - JSONArray
-        } else if (actual instanceof Response && expected instanceof JSONArray) {
+        }
+        // Response - JSONArray
+        else if (actual instanceof Response && expected instanceof JSONArray) {
             assertEquals((JSONArray) expected, new JSONArray(actual.toString()), mode, ignore);
-
-            // Response - JSONObject
-        } else if (actual instanceof Response && expected instanceof JSONObject) {
+        }
+        // Response - JSONObject
+        else if (actual instanceof Response && expected instanceof JSONObject) {
             assertEquals((JSONObject) expected, new JSONObject(actual.toString()), mode, ignore);
-
-            // Response - JSONObject[]
-        } else if (actual instanceof Response && expected instanceof JSONObject[]) {
+        }
+        // Response - JSONObject[]
+        else if (actual instanceof Response && expected instanceof JSONObject[]) {
             assertEquals(objectsToArray((JSONObject[]) expected), new JSONArray(actual.toString()), mode, ignore);
         } else {
             throw new IllegalArgumentException("Error arguments passed");
         }
-    }
-
-    private JSONArray objectsToArray(JSONObject[] jsonObjects) {
-        JSONArray expectedArray = new JSONArray();
-
-        for (JSONObject j : jsonObjects) {
-            expectedArray.put(j);
-        }
-
-        return expectedArray;
     }
 
     private void assertEquals(JSONObject expected, JSONObject actual, CompareMode mode, String[] ignore) {
@@ -75,7 +68,7 @@ class AssertionDirector {
                     .concat("\n\n")
                     .concat("Was expected: " + expected.toString(4))
                     .concat("\n\n")
-                    .concat("But found: " + actual.toString(4));
+                    .concat("But found:    " + actual.toString(4));
 
             throw new AssertionError(message);
         } catch (JSONException e) {
@@ -89,7 +82,7 @@ class AssertionDirector {
         JSONCompareMode jsonCompareMode = CompareMode.getCompareMode(mode);
 
         if (mode.isExtensibleArray()) {
-            actual = Cutter.getEntryArray(expected, actual);
+            actual = getEntryArray(expected, actual);
         }
 
         try {
@@ -99,11 +92,71 @@ class AssertionDirector {
                     .concat("\n\n")
                     .concat("Was expected: " + expected.toString(4))
                     .concat("\n\n")
-                    .concat("But found: " + actual.toString(4));
+                    .concat("But found:    " + actual.toString(4));
 
             throw new AssertionError(message);
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+
+    private JSONArray objectsToArray(JSONObject[] jsonObjects) {
+        JSONArray expectedArray = new JSONArray();
+
+        for (JSONObject j : jsonObjects) {
+            expectedArray.put(j);
+        }
+
+        return expectedArray;
+    }
+
+    private JSONArray getEntryArray(JSONArray expected, JSONArray actual) {
+        JSONArray newArray = new JSONArray();
+        HashSet<CustomJSONObject> actualSet = new LinkedHashSet<>();
+
+        for (Object ao : actual) {
+            CustomJSONObject actualJson = new CustomJSONObject(ao);
+            for (Object eo : expected) {
+                CustomJSONObject expectedJson = new CustomJSONObject(eo);
+                if (actualJson.equals(expectedJson)) {
+                    actualSet.add(expectedJson);
+                }
+            }
+        }
+        for (CustomJSONObject o : actualSet) {
+            newArray.put(o.toJSONObject());
+        }
+
+        return newArray;
+    }
+
+    private class CustomJSONObject {
+
+        private Object o;
+
+        CustomJSONObject(Object o) {
+            this.o = o;
+        }
+
+        JSONObject toJSONObject() {
+            return new JSONObject(this.o.toString());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.o.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj != null
+                    && obj instanceof CustomJSONObject
+                    && ((CustomJSONObject) obj).o.toString().equals(this.o.toString());
+        }
+
+        @Override
+        public String toString() {
+            return this.o.toString();
         }
     }
 }
--- src/main/java/com/mbi/Cutter.java
+++ src/main/java/com/mbi/Cutter.java
@@ -3,9 +3,6 @@ package com.mbi;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-
 /**
  * Created by mbi on 8/18/16.
  */
@@ -30,55 +27,5 @@ class Cutter {
         }
 
         return result;
-    }
-
-    static JSONArray getEntryArray(JSONArray expected, JSONArray actual) {
-        JSONArray newArray = new JSONArray();
-        HashSet<MyJSONObject> actualSet = new LinkedHashSet<>();
-
-        for (Object ao : actual) {
-            MyJSONObject actualJson = new MyJSONObject(ao);
-            for (Object eo : expected) {
-                MyJSONObject expectedJson = new MyJSONObject(eo);
-                if (actualJson.equals(expectedJson)) {
-                    actualSet.add(expectedJson);
-                }
-            }
-        }
-        for (MyJSONObject o : actualSet) {
-            newArray.put(o.toJSONObject());
-        }
-
-        return newArray;
-    }
-
-    private static class MyJSONObject {
-
-        private Object o;
-
-        MyJSONObject(Object o) {
-            this.o = o;
-        }
-
-        JSONObject toJSONObject() {
-            return new JSONObject(this.o.toString());
-        }
-
-        @Override
-        public int hashCode() {
-            return this.o.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return obj != null
-                    && obj instanceof MyJSONObject
-                    && ((MyJSONObject) obj).o.toString().equals(this.o.toString());
-        }
-
-        @Override
-        public String toString() {
-            return this.o.toString();
-        }
     }
 }
